b644445cd7a487a5a94cd2c770ced83af7ea0817
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-11-13 10:52:12 -0600

    changed form

--- src/Form.js
+++ src/Form.js
@@ -1,33 +1,17 @@
-import './form.css'
+import "./form.css";
 
 export default function Form() {
   return (
-    <form 
-      method='POST' 
-      name='contactform'
-      className='contactForm'>
-    
-      <input 
-        type='hidden' 
-        name='form-name'
-        value='contactForm' />
+    <form method="POST" name="contactForm" className="contactForm">
+      <input type="hidden" name="form-name" value="contactForm" />
 
-      <input 
-        type='text' 
-        name='name' 
-        placeholder='Enter your name' />
+      <input type="text" name="name" placeholder="Enter your name" />
 
-      <input 
-        type='email' 
-        name='email' 
-        placeholder='Enter your email' />
+      <input type="email" name="email" placeholder="Enter your email" />
 
-      <textarea 
-        name='message' 
-        placeholder='Messaage'></textarea>
+      <textarea name="message" placeholder="Messaage"></textarea>
 
-      <button type='submit'>Submit</button>
-
+      <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
